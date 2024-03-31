f48fcdd172b5b104eb7cea6510cf26fb1723c990
avocitty <165597853+avocitty@users.noreply.github.com>
2024-04-01 05:53:33 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    if (window.scrollY > 500) {
-        navbar.style.opacity = '1';
-    } else {
-        navbar.style.opacity = '.5';
-    }
-});
